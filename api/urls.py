b196cf2da480e7abe99e3995c6cda6bb57ef2cab
Artem Viktorovich Levkivskyi <artem.levkivskyi@gmail.com>
2020-11-05 08:26:56 +0200

    init commit

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import manage_items, manage_item
+
+urlpatterns = {
+    # if you work with all values
+    path('items', manage_items, name="items"),
+    # if you work with single value
+    path('items/<slug:key>', manage_item, name="single_item")
+}
+
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ api/views.py
@@ -0,0 +1,77 @@
+import json
+from django.conf import settings
+import redis
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+# Connect to REDIS server
+redis_instance = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
+
+
+# Operations which Django doing, if you don`t use key in request
+# (.../api/items)
+@api_view(['GET', 'POST'])
+def manage_items(request, *args, **kwargs):
+    # POST request - if you want to create new row in REDIS storage
+    if request.method == 'POST':
+        item = json.loads(request.body)
+        key = list(item.keys())[0]
+        value = item[key]
+        redis_instance.set(key, value)
+        response = {'MESSAGE': f"{value} successfully set to {key}"}
+        return Response(response, 200)
+
+    # GET request - if you want to get list with all rows in REDIS storage
+    elif request.method == 'GET':
+        items = {}
+        count = 0
+        for key in redis_instance.keys("*"):
+            items[key.decode("utf-8")] = redis_instance.get(key)
+            count += 1
+        response = {
+            'count': count,
+            'MESSAGE': f"Found {count} items.",
+            'items': items
+        }
+        return Response(response, status=200)
+
+
+# Operations which Django doing, if you use key in request
+# (.../api/items/{key})
+@api_view(['GET', 'DELETE'])
+def manage_item(request, *args, **kwargs):
+
+    # GET request - if you want to get value from REDIS for your key
+    if request.method == 'GET':
+        if kwargs['key']:
+            value = redis_instance.get(kwargs['key'])
+            if value:
+                response = {
+                    'key': kwargs['key'],
+                    'value': value,
+                    'msg': 'success'
+                }
+                return Response(response, status=200)
+            else:
+                response = {
+                    'key': kwargs['key'],
+                    'value': None,
+                    'msg': 'Not found'
+                }
+                return Response(response, status=404)
+
+    # DELETE request - if you want to delete row with key from REDIS
+    elif request.method == 'DELETE':
+        if kwargs['key']:
+            result = redis_instance.delete(kwargs['key'])
+            if result == 1:
+                response = {'MESSAGE': f"{kwargs['key']} successfully deleted"}
+                return Response(response, status=200)
+            else:
+                response = {
+                    'key': kwargs['key'],
+                    'value': None,
+                    'msg': 'Not found'
+                }
+                return Response(response, status=404)
